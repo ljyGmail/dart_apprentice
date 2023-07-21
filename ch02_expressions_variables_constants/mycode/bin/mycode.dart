34dbc61fb90b07c7380f0b58f2b78b103414f276
liangjinyong <liangjy881@gmail.com>
2023-07-21 11:34:23 +0900

    ch02 expressions, variables and constants

---
+++ ch02_expressions_variables_constants/mycode/bin/mycode.dart
@@ -0,0 +1,124 @@
+import 'dart:math';
+// # Commenting Code
+
+// This is a comment. It is not executed.
+
+// This is also a comment,
+// over multiple lines.
+
+/* This is also a comment. Over many...
+many...
+many lines. */
+
+/* This is a comment.
+/* And inside it is
+another comment. */
+Back to the first. */
+
+/// I am a documentation comment
+/// at your service.
+
+/// Me, too!
+
+void main() {
+  // ## Printing Output
+  print('Hello, Dart Apprentice reader!');
+
+  // # Statements and Expressions
+
+  // ## Statements
+  print("Hello, Dart Apprentice reader!");
+
+  // ## Expressions
+  print(42);
+  print(3 + 2);
+  print('Hello, Dart Apprentice reader!');
+  const x = 3;
+  print(x);
+
+  // # Arithmetic Operations
+  // ## Simple Operations
+  print(2 + 6);
+  print(10 - 2);
+  print(2 * 4);
+  print(24 / 3);
+
+  // ## Decimal Numbers
+  print(22 / 7);
+  print(22 ~/ 7); // truncating division operator
+
+  // ## The Euclidean Modulo Operation
+  print(28 % 10);
+
+  // ## Order of Operations
+  print(350 / 5 + 2);
+  print(350 / (5 + 2));
+
+  // ## Math Functions
+  print(sin(45 * pi / 180));
+  print(cos(135 * pi / 180));
+  print(sqrt(2));
+  print(max(5, 10));
+  print(min(-5, -10));
+  print(max(sqrt(2), pi / 2));
+
+  // # Naming Data
+  // ## Variables
+  int number = 10;
+  number = 15;
+  print(number);
+
+  double apple = 3.14159;
+  print(apple);
+
+  print(10.isEven); // => true
+  print(3.14159.round()); // => 3
+
+  // ## Type Safety
+  int myInteger = 10;
+  // myInteger = 3.14159; // No, no, no. That's not allowed :]
+
+  num myNumber;
+  myNumber = 10; // OK
+  myNumber = 3.14159; // OK
+  // myNumber = 'ten'; // No, no, no.
+
+  dynamic myVariable;
+  myVariable = 10; // OK
+  myVariable = 3.14159; // OK
+  myVariable = 'ten'; // OK
+
+  // ## Type Inference
+  var someNumber = 10;
+  someNumber = 5; // OK
+  // someNumber = 3.14159; // No, no, no.
+
+  // ## Constants
+  // ### Const Constants
+  const myConstant = 10;
+  // myConstant = 0; // Not allowed.
+
+  // ### Final Constants
+  final hoursSinceMidnight = DateTime.now().hour;
+  // hoursSinceMidnight = 0; // Not allowed.
+
+  // ## Using Meaningful Names
+
+  // # Increment and Decrement
+  var counter = 0;
+
+  counter += 1; // -> counter = counter + 1;
+
+  counter -= 1; // -> counter = counter - 1;
+
+  counter++;
+  counter--;
+
+  double myValue = 10;
+
+  myValue *= 3; // same as myValue = myValue * 3;
+  // myValue = 30.0;
+
+  myValue /= 2; // same as myValue = myValue / 2;
+  // myValue = 15.0;
+}
